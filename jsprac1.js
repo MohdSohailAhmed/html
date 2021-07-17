7b36370594546188d4616d72c4a4253c3670c54d
MohdSohailAhmed <63660125+MohdSohailAhmed@users.noreply.github.com>
2021-07-17 12:26:18 +0530

    Add files via upload

---
+++ jsprac1.js
@@ -0,0 +1,6 @@
+var firstName= prompt("Enter your first name");
+var lastName= prompt("Enter your Last name");
+var age= prompt("Enter your age");
+var fullName= firstName + " " + lastName;
+console.log("Your full name is " + fullName);
+console.log("Your age is " + age);
